0614e8f55020643787f834db0482aea1f77b6f12
rewenset <a.zakurenyi@gmail.com>
2017-01-28 11:21:15 +0200

    Added a comment system for the blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 
 
 class PostListView(ListView):
@@ -17,6 +18,20 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'comment_form': comment_form})
+
